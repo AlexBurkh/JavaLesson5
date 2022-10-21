8b7542318fd97e1c3fcfd5a9f2794269fc49574b
Alex <alex.burkh@yandex.ru>
2022-10-21 17:22:37 +0300

    Выполнена задача 1

---
+++ Program.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+public class Program {
+    public static void main(String[] args) {
+        Map<Integer, Contact> contacts = new HashMap<>();
+        String[] names = new String[] {"Алексей", "Дмитрий", "Игорь"};
+        String[] numbers = new String[] {"123", "456", "789"};
+
+        for (int i = 0; i < names.length; i++) {
+            contacts.put(i, new Contact(names[i], numbers[i]));
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            contacts.get(i).addNumber("111");
+        }
+
+        for (int i = 0; i < contacts.size(); i++) {
+            System.out.println(contacts.get(i));
+        }
+    }
+}
